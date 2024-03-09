39dc11bf68f2eac3157b1bbb97f4ab42cd7894f0
pc <pc@DESKTOP-34S7PIR>
2024-03-09 10:16:20 -0600

    Agregar archivos de proyecto.

---
+++ CLASE_09_3_2024/Program.cs
@@ -0,0 +1,136 @@
+﻿//static void suma ()
+//{
+//    int respuesta, val1, val2 = 0;
+
+//    Console.WriteLine("ingrese el valor 1");
+//    val1 = int.Parse(Console.ReadLine());
+
+//    Console.WriteLine("ingrese el valor 2");
+//    val2 = int.Parse(Console.ReadLine());
+//    respuesta = val1 + val2;
+//    Console.WriteLine("La suma es: " + respuesta);
+//}
+
+//static void datos ()
+//{
+//    Console.WriteLine("ingrese su nombre");
+//    string nombre = Console.ReadLine();
+
+//    Console.WriteLine("Hola un gusto " + nombre);
+//}
+
+//static void nacimiento ()
+//{
+
+//    Console.WriteLine("ingrese su año de nacimiento");
+//    int año = int.Parse(Console.ReadLine());
+//    int edad = 2024 - año;
+//    Console.WriteLine("su edad es: " + edad);
+
+//}
+
+//int opc;
+//do
+//{
+//    Console.WriteLine("que opcion necesita:");
+//    Console.WriteLine("opcion 1 = suma");
+//    Console.WriteLine("opcion 2 = nombre");
+//    Console.WriteLine("opcion 3 = año");
+//    Console.WriteLine("opcion 4 = salir");
+
+
+//     opc = int.Parse(Console.ReadLine());    
+//    switch (opc){
+//        case 1:
+//            suma();
+//            break;
+//        case 2:
+//            datos();
+//            break;
+//        case 3:
+//            nacimiento();
+//            break;
+//        case 4:
+//            Console.WriteLine("CERRANDO PROGRAMA");
+//            break;
+//        default:
+//            Console.WriteLine("ERROR DE FORMATO");
+//            break;
+//    }
+//}while (opc!= 4);
+
+
+//static int suma1 (int val1, int val2, int val3)
+//{
+//    return val1 + val2 + val3;   
+    
+   
+//}
+
+//int promedio = suma1(30, 20, 40) / 3;
+//Console.WriteLine(promedio);
+//promedio = suma1(50, 60, 90);
+
+
+
+//static string datos1 (string nombre)
+//{
+//    nombre = Console.ReadLine ();
+//    return nombre;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
